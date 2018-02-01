1106ac4d91055e70dc7cebde1f585f465259989a
Rodrigo Pacheco Martinez Atienza <rpacheco@alpha29.aulas.gsyc.urjc.es>
2018-02-01 11:01:53 +0100

    Segunda versión calculadora.py - Mejor tratamiento de excepciones

--- calculadora.py
+++ calculadora.py
@@ -19,13 +19,17 @@ def dividir(a,b):
 operaciones = {'suma': sumar, 'resta': restar, 'multiplica': multiplicar, 'divide': dividir}
 param = argv
 
-print("Opciones de calculadora:\n", "- Suma\n", "- Resta\n", "- Multiplica\n", "- Divide\n")
+print("\nUso: python3 calculadora.py 'opción' valor1 valor2\n\n", "Opciones de calculadora:\n", "- Suma\n", "- Resta\n", "- Multiplica\n", "- Divide\n")
 
 try:
-    resultado = operaciones[param[1]](int(param[2]), int(param[3]))
+    resultado = operaciones[param[1]](float(param[2]), float(param[3]))
     print("Resultado: ", resultado)
 except IndexError:
     print("Número de parámetros incorrecto")
 except KeyError:
     print("Opción no soportada")
+except ZeroDivisionError:
+    print("División por 0. Resultado es 0")
+except ValueError:
+    print("Valor introducido erróneo. Debe ser número")
 
